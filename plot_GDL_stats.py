6b19dba4a9782f296cf5496dc07b4d324c2de0c4
stelachance <62811207+stelachance@users.noreply.github.com>
2020-04-27 10:15:27 -0400

    Update plot_GDL_stats.py
    Modules ajoutés : 
    - Options d'enregistrement du graphique
    - Graphique de type trn_vs_val_loss.

--- plot_GDL_stats.py
+++ plot_GDL_stats.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Create plots using GeoDeepLearning's training results.
+Create plot using GeoDeepLearning's training results.
 
-- folder_path (string) : Path of the folder containing GeoDeepLearning's 
-    training results. May contain multiple sub-folders, each representing one
-    set of training results to plot.
-- graph_type (array) : Type of plots to produce
+- inFolders (string) : Path(s) of the folder(s) containing GeoDeepLearning's 
+    training results.
+- graph_type (array) : Type of plot to produce.
+
+Only one plot is produced at a time.
     
 Created by Stéphanie Lachance (2020-04-07).
 """
@@ -13,11 +14,10 @@ Created by Stéphanie Lachance (2020-04-07).
 import argparse
 import os.path
 
-#import glob
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 
+
 def retrieve_param_value(path, GDLparam):
     '''
     Extract the learning rate of the GDL training session from a file path.
@@ -150,8 +150,54 @@ def val_stat_vs_epoch (folder, files, stat_name):
         
         return new_df
     
-    df1.sort_values(by='epoch', inplace=True)          
+    # # To sort values with a specific parametter
+    # df1.sort_values(by='epoch', inplace=True)  
+    # return df1.loc[df1['epoch'] < 16]
+        
     return df1
+
+
+def trn_vs_val_loss(folder):
+    '''
+    Plot of the training and validation losses for each epoch. Helps to identify
+    overfitting.
+    
+    param folder (array of string): Folder containing the results of the GDL training session.
+    '''
+    
+    # Retrieve average values
+    data = []
+    with open(os.path.join(folder, 'metric_trn_loss.log'), 'r') as f:
+        log = f.readlines()
+                
+        for line in log:
+            cols = (line.replace('\n', '')).split('\t')
+            data.append([int(cols[0]), float(cols[1])])
+    
+    # Save values in dataframe
+    df1 = pd.DataFrame(data, columns=['epoch', 'trn_loss'])
+    
+    
+    # Retrieve values per class (if exist)
+    data = []
+
+    with open(os.path.join(folder, 'metric_val_loss.log'), 'r') as f:
+        log = f.readlines()
+                
+        for line in log:
+            cols = (line.replace('\n', '')).split('\t')
+            data.append([int(cols[0]), float(cols[1])])
+    
+    # Save values in dataframe
+    df2 = pd.DataFrame(data, columns=['epoch', 'val_loss'])
+    
+    # Merge dataframes
+    df2 = df2.set_index('epoch')
+    new_df = df1.join(df2, on='epoch')
+    new_df.sort_values(by='epoch', inplace=True)
+        
+    return new_df
+
 
 def plot_data(df, title, x_name, y_name, style):
     '''
@@ -163,7 +209,7 @@ def plot_data(df, title, x_name, y_name, style):
     y_name (string) : Name of the vertical axis.
     style (dictionnary) : Set of parameters related to the plot's appearence.
     return: None
-    '''        
+    '''    
     nb_cols = len(df.columns)
     
     # Data extent
@@ -180,7 +226,7 @@ def plot_data(df, title, x_name, y_name, style):
         cols_maxs = (df.iloc[:, 1:]).max(axis=0)
         ymin, ymax = min(cols_mins), max(cols_maxs)
         extent = abs(ymax - ymin)
-        ymax = ymax + 0.1*extent
+        ymax = ymax + 0.2*extent
         ymin = ymin - 0.05*extent
         if ymin < 0:
             ymin = 0
@@ -200,27 +246,36 @@ def plot_data(df, title, x_name, y_name, style):
     plt.axis([xmin, xmax, ymin, ymax])
     if style['xscale'] is not None:
         plt.xscale(style['xscale'])
-    plt.show()
+    
+    # Saving the plot, if wanted
+    if style['output_path'] is not None :
+        # extension = os.path.splitext(style['output_path'])[1]
         
+        # if extension is not None:
+        plt.savefig(style['output_path'])
+        # else:
+        #     plt.savefig(style['output_path'], format='png')
+            
+        print('File saved.')
+     
+    plt.show() 
 
 def main(args):
-    
+    # Plot style
     plot_style = {}
     plot_style['xextent'] = args.xextent
     plot_style['yextent'] = args.yextent
     plot_style['xscale'] = None
+    plot_style['output_path'] = args.save
     
     # Create plots
     if 'loss_vs_learning_rate' in args.plots:
         plot_style['xscale'] = 'log'
         dataframe = loss_vs_learning_rate(args.inFolders)
         plot_data(dataframe, 'Best learning rate', 'Learning rate', 'Loss', plot_style)
-        
-        # Reset plot style
-        plot_style['xscale'] = None
     
     if 'stat_vs_epoch' in args.plots:
-        # Identify the files for the statistic to plot
+        # Identifying the files containing the statistic to plot
         if args.stat == 'val_fscore':
             files = ['metric_val_fscore_averaged.log', 'metric_classwise_val_fscore.log']
             title, yname = 'F-score during the validation', 'F-score'   
@@ -237,17 +292,20 @@ def main(args):
             files = ['metric_val_recall_averaged.log', 'metric_classwise_val_recall.log']
             title, yname = 'Recall during the validation', 'Recall'
         
-        # Create plots
-        for folder in args.inFolders :               
-            dataframe = val_stat_vs_epoch(folder, files, args.stat)   
-            plot_data(dataframe, title, 'Epochs', yname, plot_style)
-            
+        # Create plot               
+        dataframe = val_stat_vs_epoch(args.inFolders[0], files, args.stat)   
+        plot_data(dataframe, title, 'Epochs', yname, plot_style)
+     
+    if 'trn_vs_val_loss' in args.plots:
+        dataframe = trn_vs_val_loss(args.inFolders[0])   
+        plot_data(dataframe, 'Training and validation losses', 'Epochs', 'Loss', plot_style)
+        
             
 if __name__ == '__main__':
     # Parser
     parser = argparse.ArgumentParser(description="Sample preparation")
     parser.add_argument("plots", 
-                        choices=['loss_vs_learning_rate', 'val_stat_vs_epoch'],
+                        choices=['loss_vs_learning_rate', 'val_stat_vs_epoch', 'trn_vs_val_loss'],
                         help="Type of plot wanted.")
     parser.add_argument("inFolders", metavar="DIR", nargs='*',
                         help="Path(s) of the folder(s) containing GeoDeepLearning's training results.")
@@ -258,13 +316,29 @@ if __name__ == '__main__':
                         help="Minimum and maximum of the horizontal axis.")
     parser.add_argument("-y", "--yextent", metavar="YVALUE",nargs=2, type=float,
                         help="Minimum and maximum of the vertical axis.")
-    parser.add_argument("-sv", "--save", action="store_true", 
-                        help="If called, save plots in the folder(s).")
+    parser.add_argument("-s", "--save", metavar="DIR", 
+                        help="Output path for the plot.")
+    parser.add_argument("-r", "--replace", action="store_true", default=False, 
+                        help="If used, will replace the existing file of the same name.")
     args = parser.parse_args()
     
-    # Checking validity of the folders
+    # Checking validity of the input folders
     for folder in args.inFolders:
         if not os.path.isdir(folder):
             raise FileNotFoundError(folder)
+    
+    if args.save is not None:
+        # Checking if the ouput folder exist
+        folder = os.path.dirname(os.path.abspath(args.save))
+        if not os.path.isdir(folder):
+            raise FileNotFoundError(folder)
+            
+        # Checking if the file already exist
+        extension = os.path.splitext(args.save)[1]
+        if extension == '':
+            args.save = args.save + ".png"        
+
+        if os.path.isfile(args.save) and args.replace == False:
+            raise FileExistsError(args.save)
     
     main(args)
